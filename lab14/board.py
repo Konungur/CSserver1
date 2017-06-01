c28f97a9532be9b775e65963c8026c84d1c6cf72
OKorol <demoris19@gmail.com>
2017-06-01 23:07:25 +0300

    Add files via upload

---
+++ lab14/board.py
@@ -0,0 +1,9 @@
+#class board():
+#    def __init__(self):
+#       pass
+
+def game_field():
+    game_main_field = [[], [], []]
+    for x in range(3):
+        print(game_main_field)
+game_field()
---
+++ lab14/player.py
@@ -0,0 +1,6 @@
+class player():
+    def __init__(self, p_move):
+        self.p_move = p_move
+
+    def player_move(self):
+        pass
