1b9c12c90231e755139cd328eb3c4a6936e740a0
OKorol <demoris19@gmail.com>
2017-06-05 14:09:58 +0300

    Add files via upload

---
+++ lab14/Game_ALPHA/Bot_Nulik.py
@@ -0,0 +1,13 @@
+from player import player_move
+import board
+class bot_nulik():
+    def __init__(self, move):
+        self.move = move
+
+    def bot_move(self, move):
+        """giving move to bot"""
+        if player_move == True:
+            move = True
+        pass
+
+    def put(self):
